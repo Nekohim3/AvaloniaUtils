a87b424f0cc84c2835120ebdfca3d57bfa176154
Neko <kuroneko@protonmail.ch>
2022-10-11 01:48:09 +0300

    ITreeNode Make Parent and Childs public

--- AvaloniaUtils/Utils/Trees/ITreeNode.cs
+++ AvaloniaUtils/Utils/Trees/ITreeNode.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AvaloniaUtils.Utils.Collections;
+using DynamicData.Kernel;
 
 namespace AvaloniaUtils.Utils.Trees
 {
     public interface ITreeNode<T> where T : ISelected
     {
-        T?                                      Parent { get; set; }
-        ObservableCollectionWithSelectedItem<T> Childs { get; set; }
+        public T?                                      Parent { get; set; }
+        public ObservableCollectionWithSelectedItem<T> Childs { get; set; }
 
         public void AddChild(T    child);
         public void RemoveChild(T child);
